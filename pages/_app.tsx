f6febc597f0e34ec84f14265ba3e07ab0e83c06e
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-06-21 18:03:23 +0300

    created Htag

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop</title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Htag } from "../components";
+
+const Index = (): JSX.Element => {
+	return (
+		<>
+			<Htag tag="h1">Text</Htag>
+		</>
+	);
+};
+
+export default Index;
