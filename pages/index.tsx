8931bf665ee2b75f91409ef8729dc4ca9857458a
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-06-22 23:53:44 +0300

    created button components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "../components";
+import { Htag, Button } from "../components";
 
 const Index = (): JSX.Element => {
 	return (
 		<>
 			<Htag tag="h1">Text</Htag>
+			<Button appearance="primary">Button</Button>
+			<Button appearance="ghost">Button</Button>
 		</>
 	);
 };
