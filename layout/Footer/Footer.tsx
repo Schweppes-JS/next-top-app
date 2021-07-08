93b8d7f725529c1d4cc998964c7e9dbafe9cce97
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-07-09 00:29:18 +0300

    created footer template

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
 import { FooterProps } from "./Footer.props";
-import styles from "./Sidebar.module.css";
+import cn from "classnames";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), "yyyy")} All rights reserved</div>
+			<a href="#" target="_blank">
+				Terms of use
+			</a>
+			<a href="#" target="_blank">
+				Privacy policy
+			</a>
+		</footer>
+	);
 };
