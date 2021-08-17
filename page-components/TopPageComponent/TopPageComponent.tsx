4d9025b79cbe7aa66db3d1cd192d93da717a954d
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-08-17 19:41:19 +0300

    created Product component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { Advantages, Card, HhData, Htag, P, Sort, Tag } from "../../components";
+import { Advantages, Card, HhData, Htag, P, Product, Sort, Tag } from "../../components";
 import { SortEnum } from "../../components/Sort/Sort.props";
 import { TopLevelCategory } from "../../interfaces/page.interface";
 import { sortReducer } from "./sort.reducer";
@@ -24,7 +24,7 @@ const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentPro
 				)}
 				<Sort sort={sort} setSort={setSort} />
 			</div>
-			<div>{sortedProducts && sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+			<div>{sortedProducts && sortedProducts.map((p) => <Product key={p._id} product={p} />)}</div>
 			<div className={styles.hhTitle}>
 				<Htag tag="h2">Вакансии - {page.category}</Htag>
 				{products && (
